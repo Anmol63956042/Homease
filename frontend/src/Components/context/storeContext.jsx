1bbd6ea84d6188ca56964ab24a5f7a3c795db7c9
Anmol63956042 <yanmol1813@gmail.com>
2025-10-23 15:20:50 +0530

    Update storeContext.jsx

--- frontend/src/Components/context/storeContext.jsx
+++ frontend/src/Components/context/storeContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({}); //manage cart
-  const url = "http://localhost:4000";
+  const url = "https://homeeeease-backend.onrender.com";
   const [token, setToken] = useState("");
   const [service_list, setServiceList] = useState([]);
 
