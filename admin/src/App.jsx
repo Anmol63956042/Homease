86275ccaf33461fa1a1c219f534c4655d3667fa2
Anmol63956042 <yanmol1813@gmail.com>
2025-10-23 16:45:10 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "./components/navbar/navbar";
-import Sidebar from "./components/Sidebar/Sidebar";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
 import { Route, Routes } from "react-router-dom";
 import Add from "./Pages/Add/Add";
 import List from "./Pages/List/List";
